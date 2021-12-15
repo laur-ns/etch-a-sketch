c062ebd926327128e84b6131bbf6e15314990bb9
Laurens <laurens-morales@outlook.com>
2021-12-15 20:30:50 +1300

    Set event listener on RESET button

--- script.js
+++ script.js
@@ -17,14 +17,19 @@ document.querySelector('.btnRainbow').addEventListener('click', () => {
     e.addEventListener('mousemove', randomColour);
   });
 });
+document.querySelector('.btnReset').addEventListener('click', () => {
+  squares.forEach(e => {
+    e.setAttribute('style', 'background-color: #F2F2F2');
+  });
+});
 // -- -- //
+
 setSquareListener();
 
+// -- functions -- //
 function randomColour(colour) {
   colour.target.setAttribute('style', `background-color: #${Math.floor(Math.random()*16777215).toString(16)}`);
 }
-
-// -- functions -- //
 function setSquareListener() {
   squares.forEach(e => {
     e.addEventListener('mousemove', (e) => {

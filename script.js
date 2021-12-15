cf8930bbc8a3d7dd1200c64cfde61817d0165331
Laurens <laurens-morales@outlook.com>
2021-12-15 22:20:40 +1300

    Set event handler for btnGrid

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let squares = document.querySelectorAll('.square');
 
 // -- Event listeners -- //
 document.querySelector('.setColour').addEventListener('change', (e) => {
+  squares.forEach(e => {
+  e.removeEventListener('mousemove', randomColour);
+  });
   setColour = e.target.value;
 });
 document.querySelector('.btnEraser').addEventListener('click', () => {
@@ -17,11 +20,7 @@ document.querySelector('.btnRainbow').addEventListener('click', () => {
     e.addEventListener('mousemove', randomColour);
   });
 });
-document.querySelector('.btnReset').addEventListener('click', () => {
-  squares.forEach(e => {
-    e.setAttribute('style', 'background-color: #F2F2F2');
-  });
-});
+document.querySelector('.btnReset').addEventListener('click', resetGrid);
 document.querySelector('.btnGrid').addEventListener('click', () => {
   let size = prompt('Enter the number of squares for each row');
   if (size === null) { return; }
@@ -36,15 +35,21 @@ document.querySelector('.btnGrid').addEventListener('click', () => {
 
 setSquareListener();
 
-
 // -- functions -- //
 function randomColour(colour) {
-  colour.target.setAttribute('style', `background-color: #${Math.floor(Math.random()*16777215).toString(16)}`);
+  colour.target.style.backgroundColor = 
+    `#${Math.floor(Math.random()*16777215).toString(16)}`;
+}
+function resetGrid() {
+  squares.forEach(e => {
+    e.style.backgroundColor = '#F2F2F2';
+  });
 }
 function setSquareListener() {
   squares.forEach(e => {
     e.addEventListener('mousemove', (e) => {
-      e.target.setAttribute('style', `background-color: ${setColour};`);
+      // e.target.setAttribute('style', `background-color: ${setColour};`);
+      e.target.style.backgroundColor = `${setColour}`
     });
   });
 }
@@ -52,5 +57,19 @@ function setSquares(numOfRowSquares) {
   squares.forEach(e => {
     e.remove()
   });
+  let square = document.createElement('div');
+  let parentNode = document.querySelector('.sketchBox');
+  square.setAttribute('class', 'square')
+  totalSquares = numOfRowSquares * numOfRowSquares;
+  for (let i = 0; i < totalSquares; i++) {
+    parentNode.appendChild(square.cloneNode());
+  }
+  let squareWidth = sketchBoxWidth / numOfRowSquares;
+  squares = document.querySelectorAll('.square');
+  squares.forEach(e => {
+    e.style.width =`${squareWidth}px`;
+    setColour = '#333333';
+  });
+  setSquareListener();
 }
 // -- -- //

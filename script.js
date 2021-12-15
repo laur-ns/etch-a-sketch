0d1738fbed93f0ce3fbe9d0f0914295dd2b12347
Laurens <laurens-morales@outlook.com>
2021-12-15 20:25:20 +1300

    Implement button event handlers

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 let sketchBoxWidth = document.querySelector('.sketchBox').clientWidth;
 let setColour = document.querySelector('.setColour').getAttribute('value');
+let squares = document.querySelectorAll('.square');
+
 // -- Event listeners -- //
 document.querySelector('.setColour').addEventListener('change', (e) => {
   setColour = e.target.value;
 });
-let squares = document.querySelectorAll('.square');
+document.querySelector('.btnEraser').addEventListener('click', () => {
+  squares.forEach(e => {
+    e.removeEventListener('mousemove', randomColour);
+    });
+  setColour = '#F2F2F2';
+});
+document.querySelector('.btnRainbow').addEventListener('click', () => {
+  squares.forEach(e => {
+    e.addEventListener('mousemove', randomColour);
+  });
+});
+// -- -- //
+setSquareListener();
+
+function randomColour(colour) {
+  colour.target.setAttribute('style', `background-color: #${Math.floor(Math.random()*16777215).toString(16)}`);
+}
+
+// -- functions -- //
 function setSquareListener() {
   squares.forEach(e => {
     e.addEventListener('mousemove', (e) => {
@@ -12,11 +32,3 @@ function setSquareListener() {
     });
   });
 }
-// -- -- //
-setSquareListener();
-document.querySelector('.btnEraser').addEventListener('click', () => {
-  setColour = '#F2F2F2';
-});
-document.querySelector('.btnRainbow').addEventListener('click', () => {
-  setColour = `#${Math.floor(Math.random()*16777215).toString(16)}`;
-});
